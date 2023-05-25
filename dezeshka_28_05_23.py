e0a0f7ec06c2d5a1d6acdef348475d89d409d383
dima014 <bukindima222@gmail.com>
2023-05-25 15:57:53 +0300

    Третє завдання

--- dezeshka_28_05_23.py
+++ dezeshka_28_05_23.py
@@ -23,7 +23,7 @@ print(next(gen))
 '''
 
 #2
-
+'''
 def average_closure():
     numbers = []
     def add_number(number):
@@ -39,3 +39,24 @@ add_num(10)
 add_num(5)
 add_num(100)
 print(get_average())
+'''
+
+#3
+
+import random
+
+class PasswordGenerator:
+    def __init__(self,length,characters):
+        self.length = length
+        self.characters = characters
+
+    def generate_password(self):
+        password = ''.join(random.choice(self.characters) for i in range(self.length))
+        return password
+
+password_gen = PasswordGenerator(length = 8 , characters = ":GYE^%#($Whgeuihgeew[qifjdio857345238911")
+
+print(password_gen.generate_password())
+print(password_gen.generate_password())
+print(password_gen.generate_password())
+
