b1de2f96d6708306237401c1a41659624ef1ffcc
dima014 <bukindima222@gmail.com>
2023-05-25 15:41:17 +0300

    Друге завдання

--- dezeshka_28_05_23.py
+++ dezeshka_28_05_23.py
@@ -1,5 +1,5 @@
 #1
-
+'''
 def generate_num(num):
     if num < 2:
         return False
@@ -20,3 +20,22 @@ print(next(gen))
 print(next(gen))
 print(next(gen))
 print(next(gen))
+'''
+
+#2
+
+def average_closure():
+    numbers = []
+    def add_number(number):
+        numbers.append(number)
+    def get_avg():
+        if not numbers:
+            return 0
+        return sum(numbers) / len(numbers)
+    return add_number , get_avg
+
+add_num , get_average = average_closure()
+add_num(10)
+add_num(5)
+add_num(100)
+print(get_average())
